04f41c33b5205693575e5ccf0b23508fb9da7ea3
Emilia Corrons <maemco4@gmail.com>
2021-05-02 20:52:06 -0300

    Add single test for image

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe Image, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'expiration time' do
+    it 'expires in TTL=2.hours time' do
+      image = Image.create!(author: "an author", camera: "Canon")
+      expect(image.expired?).to be false
+
+      image.created_at=8.hours.ago
+      image.save!
+
+      expect(image.expired?).to be true
+    end
+  end
 end
